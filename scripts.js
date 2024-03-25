4c185bb99d303aef60270c11b0b4c0155870b14e
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-03-25 16:02:07 -0500

    carousel on block of videos still not moving one video at atime arrows are off

--- scripts.js
+++ scripts.js
@@ -47,39 +47,43 @@ $(document).ready(function() {
       method: 'GET',
       success: function(data) {
         $('.loader').hide();
-        $('#carouselInner').empty();
-        data.forEach(function(video) {
-          let card = `
-          <div class="carousel-item">
-                <div class="row align-items-center mx-auto">
-                  <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end justify-content-lg-center">
-                    <div class="card">
-                      <img src="${video.thumb_url}" class="card-img-top" alt="Video thumbnail">
-                      <div class="card-img-overlay text-center">
-                        <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay">
-                      </div>
-                      <div class="card-body">
-                        <h5 class="card-title font-weight-bold">${video.title}</h5>
-                        <p class="card-text text-muted">${video.description}</p>
-                        <div class="creator d-flex align-items-center">
-                          <img src="${video.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
-                          <h6 class="pl-3 m-0 main-color">${video.author}</h6>
-                        </div>
-                        <div class="info pt-3 d-flex justify-content-between">
-                          <div class="rating">
-                            ${getStarRating(video.star)}
-                          </div>
-                          <span class="main-color">${video.duration}</span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+      $('#carousel-video').empty();
+      let carouselItem = $('<div class="carousel-item"></div>');
+      let row = $('<div class="row"></div>');
+      data.forEach(function(video, index) {
+        let card = `
+          <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end justify-content-lg-center">
+            <div class="card">
+              <img src="${video.thumb_url}" class="card-img-top" alt="Video thumbnail">
+              <div class="card-img-overlay text-center">
+                <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">${video.title}</h5>
+                <p class="card-text text-muted">${video['sub-title']}</p>
+                <div class="creator d-flex align-items-center">
+                 <img src="${video.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
+                 <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                </div>
+                <div class="info pt-3 d-flex justify-content-between">
+                 <div class="rating">
+                    ${getStarRating(video.star)}
+                 </div>
+                 <span class="main-color">${video.duration}</span>
                 </div>
               </div>
-          `;
-          $('#carousel-video').append(card);
-        });
-        $('.carousel-item').first().addClass('active');
+            </div>
+          </div>
+        `;
+        row.append(card);
+        if ((index + 1) % 4 === 0 || index === data.length - 1) {
+          carouselItem.append(row);
+          $('#carousel-video').append(carouselItem);
+          carouselItem = $('<div class="carousel-item"></div>');
+          row = $('<div class="row"></div>');
+        }
+      });
+      $('.carousel-item').first().addClass('active');
       },
       error: function(xhr, status, error) {
         $('.loader').hide();
@@ -98,6 +102,7 @@ $(document).ready(function() {
     }
     return stars;
   }
+  
   fetchQuotes();
   fetchVideos();
 });
