4961510b54d6529082152213c058e5151ed037ab
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-03-28 14:08:08 -0500

    looks good carousel works, buttons invisible

--- scripts.js
+++ scripts.js
@@ -31,8 +31,8 @@ $(document).ready(function(){
             `;
             $('#carousel-quote').append(item);
           });
-          $('.carousel-slick').slick('unslick'); // Remove Slick from the carousel
-        $('.carousel-slick').slick({ // Reinitialize Slick
+          $('#carousel-quote').slick('unslick'); // Remove Slick from the carousel
+        $('#carousel-quote').slick({ // Reinitialize Slick
           infinite: true,
           slidesToShow: 1,
           slidesToScroll: 1
@@ -45,9 +45,64 @@ $(document).ready(function(){
       })
     }, 2000);
   }
-  function fetchVideo() {
-    $('.loader')
+  function getStarRating(rating) {
+    let stars = '';
+    for (var i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars += '<img src="images/star_on.png" alt="star on" width="15px">';
+      } else {
+        stars += '<img src="images/star_off.png" alt="star off" width="15px">';
+      }
+    }
+    return stars;
   }
-
+  function fetchVideos() {
+    $('.loader').hide();
+    setTimeout(function() {
+      $.ajax({
+        url: 'https://smileschool-api.hbtn.info/popular-tutorials',
+        method: 'GET',
+        success: function(data) {
+          data.forEach(function(video) {
+            let videoItem = `
+              <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end justify-content-lg-center">
+                <div class="card">
+                  <img src="${video.thumb_url}" class="card-img-top" alt="Video thumbnail">
+                  <div class="card-img-overlay text-center">
+                    <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title font-weight-bold">${video.title}</h5>
+                    <p class="card-text text-muted">${video['sub-title']}</p>
+                    <div class="creator d-flex align-items-center">
+                     <img src="${video.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
+                     <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                    </div>
+                    <div class="info pt-3 d-flex justify-content-between">
+                     <div class="rating">
+                        ${getStarRating(video.star)}
+                     </div>
+                     <span class="main-color">${video.duration}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `;
+            $('#carousel-video').append(videoItem);
+        })
+        $('#carousel-video').slick('unslick'); // Remove Slick from the carousel
+        $('#carousel-video').slick({ // Reinitialize Slick
+          infinite: true,
+          slidesToShow: 4,
+          slidesToScroll: 1
+        });
+    },
+    error: function(xhr, status, error) {
+      $('.loader').hide();
+      console.error(error);
+    }
+  })}, 2000);
+  }
   fetchQuotes();
+  fetchVideos();
 });
