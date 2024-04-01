1eec81f210c8684b4abe48c76eb316f0756b9052
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-04-01 14:30:18 -0500

    dynamic content for latest videos, html structure and respnsive display with slick, yet arros disappeared

--- scripts.js
+++ scripts.js
@@ -57,12 +57,13 @@ $(document).ready(function(){
     return stars;
   }
   function fetchVideos() {
-    $('.loader').hide();
+    $('.loader').show();
     setTimeout(function() {
       $.ajax({
         url: 'https://smileschool-api.hbtn.info/popular-tutorials',
         method: 'GET',
         success: function(data) {
+          $('.loader').hide();
           data.forEach(function(video) {
             let videoItem = `
               <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end justify-content-lg-center">
@@ -123,6 +124,75 @@ $(document).ready(function(){
     }
   })}, 2000);
   }
+  function fetchLatest() {
+    $('.loader').show();
+    setTimeout(function() {
+      $.ajax({
+        url: 'https://smileschool-api.hbtn.info/latest-videos',
+        method: 'GET',
+        success: function(data) {
+          $('.loader').hide();
+          data.forEach(function(video) {
+            let videoItem = `
+              <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end justify-content-lg-center">
+                <div class="card">
+                  <img src="${video.thumb_url}" class="card-img-top" alt="Video thumbnail">
+                  <div class="card-img-overlay text-center">
+                    <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay">
+                  </div>
+                  <div class="card-body">
+                    <h5 class="card-title font-weight-bold">${video.title}</h5>
+                    <p class="card-text text-muted">${video['sub-title']}</p>
+                    <div class="creator d-flex align-items-center">
+                     <img src="${video.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
+                     <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                    </div>
+                    <div class="info pt-3 d-flex justify-content-between">
+                     <div class="rating">
+                        ${getStarRating(video.star)}
+                     </div>
+                     <span class="main-color">${video.duration}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `;
+            $('#carousel-video-latest').append(videoItem);
+        })
+        $('#carousel-video-latest').slick('unslick'); // Remove Slick from the carousel
+        $('#carousel-video-latest').slick({ // Reinitialize Slick
+          infinite: true,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+        });
+    },
+    error: function(xhr, status, error) {
+      $('.loader').hide();
+      console.error(error);
+    }
+  })}, 2000);
+  }
+  fetchLatest();
   fetchVideos();
   fetchQuotes();
   
